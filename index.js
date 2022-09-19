d5d57dcf98d4044184c2dc2e9d6bbb1395b36459
Anatolii <anatoliy.petrenko.777@gmail.com>
2022-09-19 14:19:58 +0300

    Create & print objects in index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { print } from './js/lib.js';
+import { print } from "./js/lib.js";
 /* Refer to https://github.com/OleksiyRudenko/a-tiny-JS-world for the task details
    Complete the below for code reviewers' convenience:
 
@@ -7,24 +7,60 @@ import { print } from './js/lib.js';
    */
 
 // ======== OBJECTS DEFINITIONS ========
-// Define your objects here
+const man = {
+  species: "human",
+  name: "Antony",
+  gender: "male",
+  hands: 2,
+  legs: 2,
+  saying: "Hi Bro!",
+};
 
+const woman = {
+  species: "human",
+  name: "Jeeny",
+  gender: "female",
+  hands: 2,
+  legs: 2,
+  saying: "Hello! How it's going?",
+};
 
+const dog = {
+  species: "dog",
+  name: "Toby",
+  gender: "male",
+  hands: 0,
+  legs: 4,
+  saying: "Woof-Woof!",
+};
+
+const cat = {
+  species: "cat",
+  name: "Bella",
+  gender: "female",
+  hands: 0,
+  legs: 4,
+  saying: "Meow!",
+};
+
+const catWoman = {
+  species: "human",
+  name: "JeenyCat",
+  gender: "female",
+  hands: 2,
+  legs: 2,
+  saying: cat.saying,
+};
 // ======== OUTPUT ========
-/* Use print(message) for output.
-   Default tag for message is <pre>. Use print(message,'div') to change containing element tag.
-
-   Message can contain HTML markup. You may also tweak index.html and/or styles.css.
-   However, please, REFRAIN from improving visuals at least until your code is reviewed
-   so code reviewers might focus on a single file that is index.js.
-   */
-
-/* Print examples:
-   print('ABC');
-   print('<strong>ABC</strong>');
-   print('<strong>ABC</strong>', 'div');
-
-   print('human; John; male; 2; 2; Hello world!; Rex, Tom, Jenny');
-   print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny');
-   print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny', 'div');
-   */
+const inhabitants = [man, woman, dog, cat, catWoman];
+const inhabitantsKeys = [
+  "species",
+  "name",
+  "gender",
+  "hands",
+  "legs",
+  "saying",
+];
+inhabitants.map((inhabitant) => {
+  print(inhabitantsKeys.map((key) => key + ":" + inhabitant[key]).join("\n"));
+});
