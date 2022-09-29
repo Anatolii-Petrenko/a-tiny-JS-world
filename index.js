9298ccf1678a54cb3ae9ab1e8bd8bbaf05116dd4
Anatolii <anatoliy.petrenko.777@gmail.com>
2022-09-29 20:05:18 +0300

    Create classes structure

--- index.js
+++ index.js
@@ -7,60 +7,77 @@ import { print } from "./js/lib.js";
    */
 
 // ======== OBJECTS DEFINITIONS ========
-const man = {
-  species: "human",
-  name: "Antony",
-  gender: "male",
-  hands: 2,
-  legs: 2,
-  saying: "Hi Bro!",
-};
+class Inhabitant {
+  constructor(name, gender, saying) {
+    this.name = name;
+    this.gender = gender;
+    this.saying = saying;
+  }
+  getInfo() {
+    return [
+      `Name: ${this.name}`,
+      `Gender: ${this.gender}`,
+      `Greeting: ${this.saying}`,
+    ];
+  }
+}
 
-const woman = {
-  species: "human",
-  name: "Jeeny",
-  gender: "female",
-  hands: 2,
-  legs: 2,
-  saying: "Hello! How it's going?",
-};
+class Human extends Inhabitant {
+  species = "Human";
+  hands = 2;
+  legs = 2;
+  constructor(name, gender, saying) {
+    super(name, gender, saying);
+  }
+  getInfo() {
+    return [
+      this.species,
+      ...super.getInfo(),
+      `Hands: ${this.hands}`,
+      `Legs: ${this.legs}`,
+    ];
+  }
+}
 
-const dog = {
-  species: "dog",
-  name: "Toby",
-  gender: "male",
-  hands: 0,
-  legs: 4,
-  saying: "Woof-Woof!",
-};
+class Animal extends Inhabitant {
+  constructor(name, gender, saying) {
+    super(name, gender, saying);
+  }
+  species = "Animal";
+  legs = 4;
+  tail = 1;
+  getInfo() {
+    return [
+      this.species,
+      ...super.getInfo(),
+      `Legs: ${this.legs}`,
+      `Tail: ${this.tail}`,
+    ];
+  }
+}
 
-const cat = {
-  species: "cat",
-  name: "Bella",
-  gender: "female",
-  hands: 0,
-  legs: 4,
-  saying: "Meow!",
-};
+class Cat extends Animal {
+  species = "Cat";
+  constructor(name, gender, saying, species, legs, tail) {
+    super(name, gender, saying, species, legs, tail);
+  }
+}
 
-const catWoman = {
-  species: "human",
-  name: "JeenyCat",
-  gender: "female",
-  hands: 2,
-  legs: 2,
-  saying: cat.saying,
-};
+class Dog extends Animal {
+  species = "Dog";
+  constructor(name, gender, saying, species, legs, tail) {
+    super(name, gender, saying, species, legs, tail);
+  }
+}
 // ======== OUTPUT ========
+const man = new Human("Antony", "male", "Hi Bro!");
+const woman = new Human("Jeeny", "female", "Hello! How it's going?");
+const cat = new Cat("Bella", "female", "Meow!");
+const dog = new Dog("Toby", "male", "Woof-Woof!");
+const catWoman = new Human("JeenyCat", "female", woman.saying);
+
 const inhabitants = [man, woman, dog, cat, catWoman];
-const inhabitantsKeys = [
-  "species",
-  "name",
-  "gender",
-  "hands",
-  "legs",
-  "saying",
-];
+
 inhabitants.map((inhabitant) => {
-  print(inhabitantsKeys.map((key) => key + ":" + inhabitant[key]).join("\n"));
+  print(inhabitant.getInfo().join("\n"));
 });
